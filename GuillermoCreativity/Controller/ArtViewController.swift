ceb7c3b75ade52d9c0380d982d34fd67fbf2062a
Guillermo <memo33101@gmail.com>
2018-11-07 07:55:59 -0700

    Created project

---
+++ GuillermoCreativity/Controller/ArtViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ArtViewController.swift
+//  CreativityProject
+//
+//  Created by Spaulding, Bryson on 11/1/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+}
+
